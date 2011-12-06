c347926689d40fc83fa1ee499374b1b5cb2c12f3
Peter Hanecak <hanecak@opendata.sk>
2011-12-06 10:19:21 +0000

    currencies CZK and USD now recognized

--- src/main/java/sk/opendata/odn/model/Currency.java
+++ src/main/java/sk/opendata/odn/model/Currency.java
@@ -14,8 +14,10 @@ import java.util.Map;
  * </ul>
  */
 public enum Currency {
+	CZK("CZK", "Kč"),
 	EUR("EUR", "€"),
-	SKK("SKK", "Sk");
+	SKK("SKK", "Sk"),
+	USD("USD", "$");
 
 	private String currencyCode;	// ISO 4217 - see http://en.wikipedia.org/wiki/ISO_4217
 	private String currencySign;	// see http://en.wikipedia.org/wiki/Currency_sign
